daf10eae97829f444f0af1058a73b4b8ed6f9d39
turinglabsorg <sebastiano.cataudo@gmail.com>
2022-01-21 14:41:01 +0100

    removing original package

--- packages/tznft/jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type {Config} from '@jest/types';
-
-// Sync object
-const config: Config.InitialOptions = {
-  testMatch: ['**/__tests__/*.+(spec|test).[jt]s?(x)'],
-  verbose: true,
-  preset: 'ts-jest'
-};
-export default config;
